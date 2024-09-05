dd4b4ed28d27065feb6f1b4bbe2cad1c19b4bb0e
Frans David Aguirre Rueda <dark.tempus@gmail.com>
2024-09-05 09:32:15 -0500

    services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
-import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
 
--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagesComponent } from './pages/pages.component';
 import { ComponentsComponent } from './components/components.component';
-import { ServicesComponent } from './services/services.component';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
 import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
@@ -17,7 +16,6 @@ import { CountryTableComponent } from './components/country-table/country-table.
   declarations: [
     PagesComponent,
     ComponentsComponent,
-    ServicesComponent,
     ByCapitalPageComponent,
     ByCountryPageComponent,
     ByRegionPageComponent,
