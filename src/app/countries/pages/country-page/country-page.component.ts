735a9762a1dd49c712f1b0fe7abd9e401276335c
Frans David Aguirre Rueda <dark.tempus@gmail.com>
2024-09-06 09:54:23 -0500

    country-page configuration

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { count, switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-country-page',
@@ -10,28 +11,27 @@ import { count, switchMap } from 'rxjs';
   ]
 })
 export class CountryPageComponent implements OnInit {
+  public country?: Country
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private router:Router,
+    private router: Router,
     private countriesService: CountriesService,
   ) {
 
   }
   ngOnInit(): void {
 
-this.activatedRoute.params.pipe(
-  switchMap(({ id }) => this.countriesService.searchCountryByAlphaCode(id))
+    this.activatedRoute.params
+    .pipe(
+      switchMap(({ id }) => this.countriesService.searchCountryByAlphaCode(id))
+    ).subscribe(country => {
 
+      if (!country) return this.router.navigateByUrl('');
 
-).subscribe( country =>{
+      return this.country = country
 
-if (!country){return this.router.navigateByUrl('')}
-
-console.log('Tenemos un pais ');
-return
-
-})
+    })
   }
 
 }
